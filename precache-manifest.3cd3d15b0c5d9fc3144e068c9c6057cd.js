345779bea24d44a2d59038d1b2a604f982757965
Hussein Almulla <hussenalmulla@gmail.com>
2020-03-15 13:15:34 -0400

    Updates

---
+++ precache-manifest.3cd3d15b0c5d9fc3144e068c9c6057cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33dee1da4691588ff57508f444fea31",
+    "url": "/Learning-ReactLearning-robofriends/index.html"
+  },
+  {
+    "revision": "d376a3406b6d232543a5",
+    "url": "/Learning-ReactLearning-robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "82a7b9886e9bd3c04c99",
+    "url": "/Learning-ReactLearning-robofriends/static/css/main.430a84af.chunk.css"
+  },
+  {
+    "revision": "d376a3406b6d232543a5",
+    "url": "/Learning-ReactLearning-robofriends/static/js/2.747dcac3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Learning-ReactLearning-robofriends/static/js/2.747dcac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82a7b9886e9bd3c04c99",
+    "url": "/Learning-ReactLearning-robofriends/static/js/main.e830c790.chunk.js"
+  },
+  {
+    "revision": "1fed5ea4d70e8e8ada6c",
+    "url": "/Learning-ReactLearning-robofriends/static/js/runtime-main.ed324faf.js"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/Learning-ReactLearning-robofriends/static/media/Lobster.9406d0ab.ttf"
+  }
+]);
